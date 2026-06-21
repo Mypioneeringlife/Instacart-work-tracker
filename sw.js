c6ae5de1c77a9e85f4d3370b4f23cb9f59f97b93
Mypioneeringlife <jaddambaird@gmail.com>
2026-06-20 18:04:06 -0700

    Add service worker for app install experience

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "instacart-work-tracker-v4-3-splash";
+const URLS = ["./", "./index.html", "./manifest.webmanifest", "./app-icon.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
